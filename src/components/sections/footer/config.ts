c7f6deb002b9d241510613dc3c7ad2c1b6647f03
John Francis Absalon <johnfrancisabsalon17@gmail.com>
2025-03-13 08:36:40 +0800

    feat: add footer links configuration

--- src/components/sections/footer/config.ts
+++ src/components/sections/footer/config.ts
@@ -5,3 +5,9 @@ export const copyright = {
   startYear: 2024
 };
 
+export const footer: FooterItem = {
+  links: [
+    { label: 'Home', href: '/' },
+    { label: 'About', href: '/about' },
+  ]
+};
